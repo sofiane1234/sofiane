61c46abdd84148275906d2043c7891bb3ab66ce7
Sofiane Draris <sofiane.draris@ynov.com>
2024-05-03 15:28:01 +0200

    sosso

--- src/main/java/com/ynov/sofiane/services/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.ynov.sofiane.services;
-
-import com.ynov.sofiane.models.Role;
-import com.ynov.sofiane.models.User;
-
-public interface AuthService {
-    String login(User user, String password);
-    User register(User entity, Role admin);
-}
